57e816599e3972221dcec887dd0b7777b42fcf0d
AKevra <anetkevra@mail.ru>
2020-09-15 23:09:18 +0300

    Updates

---
+++ precache-manifest.9fbffb317ac028ca9938f5dc2fed0c38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c6b6e9379e8c5a5841114c87b06dfc3",
+    "url": "/rick_and_morty_bd/index.html"
+  },
+  {
+    "revision": "fa924c3ab591a1de331d",
+    "url": "/rick_and_morty_bd/static/css/main.a57d4458.chunk.css"
+  },
+  {
+    "revision": "d7995065c9d09fda39ef",
+    "url": "/rick_and_morty_bd/static/js/2.4823626d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rick_and_morty_bd/static/js/2.4823626d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa924c3ab591a1de331d",
+    "url": "/rick_and_morty_bd/static/js/main.ee66ab46.chunk.js"
+  },
+  {
+    "revision": "bc3f104c3edcaa6bd697",
+    "url": "/rick_and_morty_bd/static/js/runtime-main.4f9f685a.js"
+  }
+]);
